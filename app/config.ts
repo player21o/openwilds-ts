103732567ff0bb81080e2a8123db54f1339e10dc
player21o <196048914+player21o@users.noreply.github.com>
2025-03-27 20:03:14 +0100

    added gitignore, added basic lobby

--- app/config.ts
+++ app/config.ts
@@ -1,7 +1,21 @@
 //this is a config
 
 export = {
-  signalling: {
-    port: 8080,
+  lobby: {
+    port: 8080, //the *localhost* port for lobby
   },
+  game: {
+    port: 8081, //the *localhost* port for game
+    url: "localhost", //the url of the game which client will connect to
+  },
+} as Config;
+
+type Config = {
+  lobby: {
+    port: number;
+  };
+  game: {
+    port: number;
+    url: string | "localhost";
+  };
 };
---
+++ app/lobby.ts
@@ -0,0 +1,25 @@
+import { WebSocketServer } from "ws";
+import config from "./config";
+
+type Packet =
+  | ["set_location", string]
+  | ["date", number]
+  | ["set_flag", string]
+  | ["ask", { token: string; question: "captcha"; request_id: number }]
+  | ["play", { noob: boolean; mode: "fun"; mmtag: string; location: string }];
+
+const wss = new WebSocketServer({ port: config.lobby.port });
+
+wss.on("connection", (ws) => {
+  const answer = (data: any) => ws.send(JSON.stringify(data));
+
+  ws.on("message", (data: Packet) => {
+    data = JSON.parse(data as any) as Packet;
+
+    switch (data[0]) {
+      case "ask":
+        if (data[1].question == "captcha")
+          answer(["response", { request_id: data[1].request_id }]); // no captcha lol
+    }
+  });
+});
