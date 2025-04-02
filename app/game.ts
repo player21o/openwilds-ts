fba7defb55dedc2d0bdc81336c591c7ae9b3f531
player21o <196048914+player21o@users.noreply.github.com>
2025-04-02 20:39:51 +0200

    migrated to uwebsockets.js, made basic outer_packet

--- app/game.ts
+++ app/game.ts
@@ -1,11 +1,28 @@
-import { decode } from "@msgpack/msgpack";
-import { App } from "uWebSockets.js";
+// game.ts
+import { decode, encode } from "@msgpack/msgpack";
+import { App, DISABLED } from "uWebSockets.js";
+import { Peer, outer_packets } from "./game/outer_packets";
 
 export class GameServer {
+  private peers: Peer[] = [];
+
   constructor(port: number) {
-    //for scalability, we don't hardcode port
     App({})
       .ws("/*", {
+        compression: DISABLED,
+        open: (ws) => {
+          this.peers.push({
+            // Add ...args to capture parameters
+            send: (msg) => {
+              // Pass args to outer_packets[msg] and include the result as the second element
+              ws.send(encode(msg), true);
+            },
+          });
+
+          this.peers[0].send(
+            outer_packets.say(true, "Hey! If you see this, everything works!")
+          );
+        },
         message: (_, msg) => {
           const packet = decode(msg);
           console.log(packet);
